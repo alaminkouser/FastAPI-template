e289bfb0d72e2fa9bf82ad9db5858b22a49a7809
AL AMIN KOUSER <alaminkouser@icloud.com>
2025-07-02 16:00:03 +0600

    added: fonts, auto add css file, navigation bar

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI, Request, Response
 from fastapi.responses import FileResponse
 from fastapi.templating import Jinja2Templates
 
-
 app = FastAPI()
 
 
@@ -12,15 +11,20 @@ home_directory = Jinja2Templates(directory="home")
 async def _(request: Request, _):
     path = request.url.path
     if path.endswith("/") and os.path.exists(f"home{path}index.html") and os.path.isfile(f"home{path}index.html"):
+        css_file = os.path.exists(f"home{path}index.css") and os.path.isfile(f"home{path}index.css")
         return home_directory.TemplateResponse(
             name="root.html",
             context={
-                "request": request
+                "request": request,
+                "css_file": css_file
             },
         )
     elif not path.endswith(".html") and os.path.exists(f"home{path}") and os.path.isfile(f"home{path}"):
         return FileResponse(
-            f"home{path}"
+            f"home{path}",
+            headers={
+                "Cache-Control": "public, max-age=3600"
+            }
         )
     else:
         return Response(
